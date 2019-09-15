2229d92ebf86bda5b13abb9d8c20fe4626017a01
mshatunov <mshatunov@gmail.com>
2019-09-15 13:10:50 +0300

    feature: spring boot initial

---
+++ src/test/java/ru/mshatunov/investments/stats/StatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mshatunov.investments.stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
