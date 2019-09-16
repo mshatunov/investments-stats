247d4e43df2c1cb2f18545028918851e69dc8da9
mshatunov <mshatunov@gmail.com>
2019-09-16 22:55:21 +0300

    refactor: rename app

--- src/main/java/ru/mshatunov/investments/stats/InvestmentStatsApplication.java
+++ src/main/java/ru/mshatunov/investments/stats/InvestmentStatsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class StatsApplication {
+public class InvestmentStatsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StatsApplication.class, args);
+        SpringApplication.run(InvestmentStatsApplication.class, args);
     }
 }
--- src/test/java/ru/mshatunov/investments/stats/InvestmentStatsApplicationTests.java
+++ src/test/java/ru/mshatunov/investments/stats/InvestmentStatsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StatsApplicationTests {
+class InvestmentStatsApplicationTests {
 
 	@Test
 	void contextLoads() {
